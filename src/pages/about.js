20930b244194bb93f238953cbc175acd7760ea45
HY <fksto0@gmail.com>
2021-08-02 17:10:08 +0900

    Initial commit for zero page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About this</title>
+      <h1>About this</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
